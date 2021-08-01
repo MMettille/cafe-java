8720995eeb4770b13f8a875e6ab6bb74b800f96a
Mary Mettille <mary.mettille.brist@gmail.com>
2021-08-01 16:09:30 -0500

    Created Nav Component/Partial and includeded it in index.php

--- admin/index.php
+++ admin/index.php
@@ -1,25 +1,4 @@
-<html>
-    <head>
-        <title>Cafe Java - Home Page</title>
-
-        <link rel="stylesheet" href="../css/admin.css">
-    </head>
-
-    <body>
-        <! -- Nav Section Starts -->
-        <div class="nav text-center">
-            <div class="wrapper">
-                <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Admin</a></li>
-                    <li><a href="">Category</a></li>
-                    <li><a href="">Menu</a></li>
-                    <li><a href="">Order</a></li>
-                </ul>
-            </div>
-
-        </div>
-        <! -- Nav Section Ends -->
+<?php include('partials/nav.php'); ?>
 
         <! -- Main Content Starts -->
         <div class="main-content">
