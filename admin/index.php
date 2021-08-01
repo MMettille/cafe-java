720de4a9e9dba6c290ecf2cb1cdaaa81a1166ff4
Mary Mettille <mary.mettille.brist@gmail.com>
2021-08-01 15:17:10 -0500

    Added  wrapper and footer text

--- admin/index.php
+++ admin/index.php
@@ -8,19 +8,25 @@
     <body>
         <! -- Menu Section Starts -->
         <div class="menu">
+            <div class="wrapper">
             Menu will go here
+            </div>
         </div>
         <! -- Menu Section Ends -->
 
         <! -- Main Content Starts -->
         <div class="main-content">
+            <div class="wrapper">
             Main Content will Go Here
+            </div>
         </div>
         <! -- Menu Section Ends -->
 
         <! -- Footer Section Starts -->
         <div class="footer">
-            Menu will go here
+            <div class="wrapper">
+            <p>All rights reserved. Designed By <a href="mmettille.github.io/portfolio">Mary Mettille</a> </p>
+            </div>
         </div>
         <! -- Footer Section Ends -->
     </body>
