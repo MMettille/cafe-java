289a9ac2f795e07b6b7f67debaa6fc54eb102794
Mary Mettille <mary.mettille.brist@gmail.com>
2021-08-01 16:07:09 -0500

    Added Manage-Admin Page - now breaking down nav and footer into componets

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -24,33 +24,8 @@
         <! -- Main Content Starts -->
         <div class="main-content">
             <div class="wrapper">
-                <h1>Dashboard</h1>
+                <h1>Manage Admin</h1>
 
-                <div class="col-4 text-center">
-                    <h1>5</h1>
-                    <br>
-                    Categories
-                </div>
-
-                <div class="col-4 text-center">
-                    <h1>5</h1>
-                    <br>
-                    Categories
-                </div>
-
-                <div class="col-4 text-center">
-                    <h1>5</h1>
-                    <br>
-                    Categories
-                </div>
-
-                <div class="col-4 text-center">
-                    <h1>5</h1>
-                    <br>
-                    Categories
-                </div>
-            </div>
-            <div class="clearfix"></div>
         </div>
         <! -- Menu Section Ends -->
 
