797429d547c07d0be598e827091fd037c69528f0
Mary Mettille <mary.mettille.brist@gmail.com>
2021-08-01 16:32:32 -0500

    Table of Employees

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -3,8 +3,27 @@
         <! -- Main Content Starts -->
         <div class="main-content">
             <div class="wrapper">
-                <h1>Manage Admin</h1>
+                <h1>Manage Employees</h1>
             </div>
+
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Full Name</th>
+                    <th>Username</th>
+                    <th>Actions</th>
+                </tr>
+
+                <tr>
+                    <td>1</td>
+                    <td>Mary M</td>
+                    <td>maryRocks</td>
+                    <td>
+                        Update
+                        Delete
+                    </td>
+                </tr>
+            </table>
         </div>
         <! -- Menu Section Ends -->
 
