0a032a9983f1c2cf6c4ba20849ac36ccbfb5cd56
Mary Mettille <mary.mettille.brist@gmail.com>
2021-08-01 13:40:50 -0500

    Roughed Out Admin Page, added Admin.css file

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,28 @@
 <html>
     <head>
         <title>Cafe Java - Home Page</title>
+
+        <link rel="stylesheet" href="../css/admin.css">
     </head>
 
     <body>
-        <h1>Admin Panel</h1>
+        <! -- Menu Section Starts -->
+        <div class="menu">
+            Menu will go here
+        </div>
+        <! -- Menu Section Ends -->
+
+        <! -- Main Content Starts -->
+        <div class="main-content">
+            Main Content will Go Here
+        </div>
+        <! -- Menu Section Ends -->
+
+        <! -- Footer Section Starts -->
+        <div class="footer">
+            Menu will go here
+        </div>
+        <! -- Footer Section Ends -->
     </body>
     
 </html>
