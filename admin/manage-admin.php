54371420ad124618d585feb52a92d69581942bb3
Mary Mettille <mary.mettille.brist@gmail.com>
2021-08-01 22:58:34 -0500

    Get Request Works

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -24,36 +24,45 @@
                         <th>Username</th>
                         <th>Actions</th>
                     </tr>
+                    
+                    <?php
+                        // SQL Query
+                        $sql = "SELECT * FROM admin";
+                        // Execute Query
+                        $res = mysqli_query($conn, $sql);
+                        // Check to see if it worked
+                        if($res == TRUE)
+                        {
+                            // Count the number of rows we have in the database
+                            $rows = mysqli_num_rows($res);
+                            // If we have more than 0 rows, 
+                            if($rows>0){
+                                while($rows=mysqli_fetch_assoc($res))
+                                {
+                                    $id=$rows['id'];
+                                    $full_name=$rows['full_name'];
+                                    $username=$rows['username'];
 
-                    <tr>
-                        <td>1</td>
-                        <td>Mary M</td>
-                        <td>maryRocks</td>
-                        <td>
-                            <a href="#" class="btn btn-secondary">Update</a>
-                            <a href="#" class="btn btn-danger">Delete</a>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>2</td>
-                        <td>Scott</td>
-                        <td>scottRocks</td>
-                        <td>
-                            <a href="#" class="btn btn-secondary">Update</a>
-                            <a href="#" class="btn btn-danger">Delete</a>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>3</td>
-                        <td>Bob</td>
-                        <td>bobRocks</td>
-                        <td>
-                            <a href="#" class="btn btn-secondary">Update</a>
-                            <a href="#" class="btn btn-danger">Delete</a>
-                        </td>
-                    </tr>
+                                    ?>
+                                        <tr>
+                                            <td><?php echo $id; ?></td>
+                                            <td><?php echo $full_name; ?></td>
+                                            <td><?php echo $username; ?></td>
+                                            <td>
+                                                <a href="#" class="btn btn-secondary">Update</a>
+                                                <a href="#" class="btn btn-danger">Delete</a>
+                                            </td>
+                                        </tr>
+                                    <?php
+                                }
+                            }
+                            else
+                            {
+                                //Do Nothing
+                            }
+                        }
+                    ?>
+        
                 </table>
             </div>
         </div>
